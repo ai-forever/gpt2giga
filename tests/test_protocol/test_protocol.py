780878b3c53040982662bbd62f0e43c74b5f1220
krakenalt <rusyak1999@gmail.com>
2025-11-05 19:00:49 +0300

    refactor: linter

--- tests/test_protocol/test_protocol.py
+++ tests/test_protocol/test_protocol.py
@@ -9,6 +9,7 @@ class DummyClient:
     def __init__(self):
         self.called = False
 
+
 def test_attachment_processor_construction():
     p = AttachmentProcessor(DummyClient(), logger)
     assert hasattr(p, "upload_image")
