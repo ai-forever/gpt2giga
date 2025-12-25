b832ed8ec4d5b4e8eb158ded6afb38f471493364
krakenalt <rusyak1999@gmail.com>
2025-12-25 15:04:22 +0300

    chore: change port in examples

--- examples/responses/single_prompt.py
+++ examples/responses/single_prompt.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(base_url="http://localhost:8000", api_key="0")
+client = OpenAI(base_url="http://localhost:8090", api_key="0")
 
 response = client.responses.create(
     input="Write a one-sentence bedtime story about a unicorn."
